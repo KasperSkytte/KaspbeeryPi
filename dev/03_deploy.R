a39bf8c26fbade26cf7d43d2aef65e3c1bea24cb
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-10-14 10:46:13 +0200

    add app.R to run kaspbeerypi::run_app()

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -29,7 +29,7 @@ devtools::build()
 ## If you want to deploy on RStudio related platforms
 #golem::add_rstudioconnect_file()
 #golem::add_shinyappsio_file()
-#golem::add_shinyserver_file()
+golem::add_shinyserver_file()
 
 ## Docker ----
 ## If you want to deploy via a generic Dockerfile
